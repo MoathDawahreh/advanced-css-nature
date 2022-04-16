cf2f0b99b0d828fc66678b2eaee13093f7bf4d52
MoathDawahreh <moath.dawahreh@gmail.com>
2022-04-17 02:26:47 +0300

    flex-grid-media-queries

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
-const handler=(element, cl)=>{
-    var elementChildren = element.querySelectorAll('.Element-child');
-    console.log('clickeeed element is: ',element); 
- element.style.opacity= element.style.opacity == 1 ? 0.5 : 1;
-//  element.addEventListener('click', function handleClick(event) {
-//     // 👇️ "parent"
-//     // console.log(event.target.style)
-//     // event.target.parentElement.style.opacity= event.target.parentElement.style.opacity == 1 ? 0.5 : 1;
+const handler = (element, cl) => {
+	var elementChildren = element.querySelectorAll('.Element-child')
+	console.log('clickeeed element is: ', element)
+	element.style.opacity = element.style.opacity == 1 ? 0.5 : 1
+	//  element.addEventListener('click', function handleClick(event) {
+	//     // 👇️ "parent"
+	//     // console.log(event.target.style)
+	//     // event.target.parentElement.style.opacity= event.target.parentElement.style.opacity == 1 ? 0.5 : 1;
 
-//  });
-//  document.getElementsByName('box-2')[0].style.opacity=1
+	//  });
+	//  document.getElementsByName('box-2')[0].style.opacity=1
 }
